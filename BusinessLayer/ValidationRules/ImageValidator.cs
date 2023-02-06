58ff33f31840c8103cb1f9738cbb192ed88216e3
Tolga Yıldız <tolgayildiz91@hotmail.com.tr>
2023-02-06 23:49:01 +0300

    Image sınıfı işlemleri yapıldı

---
+++ BusinessLayer/ValidationRules/ImageValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ImageValidator : AbstractValidator<Image>
+    {
+        public ImageValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Görsel Başlığını Boş Geçemezsiniz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Görsel Başlığını Boş Geçemezsiniz");
+            RuleFor(x => x.ImageURL).NotEmpty().WithMessage("Görsel Yolunu Boş Geçemezsiniz");
+            RuleFor(x => x.Title).MaximumLength(20).WithMessage("Lütfen En Fazla 20 Karakter Veri Girişi Yapınız");
+            RuleFor(x => x.Title).MinimumLength(8).WithMessage("Lütfen En Az 8 Karakter Veri Girişi Yapınız");
+            RuleFor(x => x.Description).MaximumLength(50).WithMessage("Lütfen En Fazla 50 Karakter Veri Girişi Yapınız");
+            RuleFor(x => x.Description).MinimumLength(20).WithMessage("Lütfen En Az 20 Karakter Veri Girişi Yapınız");
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20230206104119_mig_add_image_columns.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class migaddimagecolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Images");
+        }
+    }
+}
