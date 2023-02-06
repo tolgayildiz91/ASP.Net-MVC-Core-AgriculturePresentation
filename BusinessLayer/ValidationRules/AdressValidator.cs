ea421cebbd8a130a01c77ce765995aeaa8e8bd66
Tolga Yıldız <tolgayildiz91@hotmail.com.tr>
2023-02-07 02:34:20 +0300

    Adres sınıfı için controller, validation işlemleri yapıldı view oluşturuldu.

---
+++ BusinessLayer/ValidationRules/AdressValidator.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AdressValidator:AbstractValidator<Adress>
+    {
+        public AdressValidator()
+        {
+            RuleFor(x => x.Description1).NotEmpty().WithMessage("Açıklama 1 Boş Geçilemez");
+            RuleFor(x => x.Description1).MaximumLength(25).WithMessage("Lütfen Açıklamayı Kısalt");
+            RuleFor(x => x.Description2).NotEmpty().WithMessage("Açıklama 2 Boş Geçilemez");
+            RuleFor(x => x.Description2).MaximumLength(25).WithMessage("Lütfen Açıklamayı Kısalt");
+            RuleFor(x => x.Description3).NotEmpty().WithMessage("Açıklama 3 Boş Geçilemez");
+            RuleFor(x => x.Description3).MaximumLength(25).WithMessage("Lütfen Açıklamayı Kısalt");
+            RuleFor(x => x.Description4).NotEmpty().WithMessage("Açıklama 4 Boş Geçilemez");
+            RuleFor(x => x.Description4).MaximumLength(25).WithMessage("Lütfen Açıklamayı Kısalt");
+            RuleFor(x => x.Mapinfo).NotEmpty().WithMessage("Harita Bilgisi Boş Geçilemez");
+
+
+
+        }
+
+    }
+}
